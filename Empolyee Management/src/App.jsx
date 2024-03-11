fb6c7f52e4cdd931bc73af7d0239c2287b361144
mwaniki-Titus <mwanikititus1@gmail.com>
2024-03-10 19:22:49 -0700

    first commit

---
+++ Empolyee Management/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.scss'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <h1>pop</h1>
+    </>
+ 
+  )
+  }
+export default App
---
+++ Empolyee Management/src/main.jsx
@@ -0,0 +1,9 @@
+
+import App from './App.jsx'
+import './index.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
